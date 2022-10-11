74a125d5724ebf1fa3ff1b434d2c0396d2df8c04
wjqwjqw <99686154+wjqwjqw@users.noreply.github.com>
2022-10-11 11:58:12 +0800

    Initial commit

---
+++ src/main/java/com/wjq/psringcloud_sentinel/PsringcloudSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.wjq.psringcloud_sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsringcloudSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsringcloudSentinelApplication.class, args);
+    }
+
+}
